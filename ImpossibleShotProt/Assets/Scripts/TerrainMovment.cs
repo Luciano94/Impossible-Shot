899b42b0d01142798323e00a2a34119f2d0971e9
Unknown <lucho.elp40@gmail.com>
2018-08-25 15:52:39 -0300

    v0.2-Endless Terrain
    -Added the endless Terrain

---
+++ ImpossibleShotProt/Assets/Scripts/TerrainMovment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainMovment : MonoBehaviour {
+
+	private float distance = 0;
+	[SerializeField] private GameObject terrain;
+	[SerializeField] private float speed;
+
+	void Update () {
+		distance = terrain.transform.position.z;
+		if(distance > -100)
+		{
+			terrain.transform.Translate((Vector3.back * speed * Time.deltaTime));
+		}else
+		{
+			distance = 199;
+			terrain.transform.Translate(Vector3.forward * (distance));
+		}
+	}
+}
