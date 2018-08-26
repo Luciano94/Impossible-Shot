07fb9a304c00f13876fc79d1d20ffc9494adf9f8
Unknown <lucho.elp40@gmail.com>
2018-08-25 21:55:54 -0300

    v0.3-GameManager
    -Added the Game Manager
    -Added Level Flow

--- ImpossibleShotProt/Assets/Scripts/GameManager.cs
+++ ImpossibleShotProt/Assets/Scripts/GameManager.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public static GameManager instance;
+
+	public static GameManager Instance
+	{
+		get
+		{
+			instance = FindObjectOfType<GameManager>();
+			if(instance == null)
+			{
+				GameObject go = new GameObject("GameManager");
+				instance = go.AddComponent<GameManager>();
+			}
+			return instance;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	[SerializeField] private float terrainSpeed = 20;
+	[SerializeField] private float speedPerLevel = 10;
+	[SerializeField] private float timePerLevel = 30;
+	private float timeCurrentLevel;
+
+	public float TerrainSpeed
+	{
+		get
+		{
+			return terrainSpeed;
+		}
+	}
+
+	private void Awake()
+	{
+		timeCurrentLevel = 0;
+	}
+
+	private void Update()
+	{
+		timeCurrentLevel += Time.deltaTime;
+		if(timeCurrentLevel >= timePerLevel)
+		{
+			timeCurrentLevel = 0;
+			terrainSpeed += speedPerLevel;
+		}
 	}
 }
--- ImpossibleShotProt/Assets/Scripts/TerrainMovment.cs
+++ ImpossibleShotProt/Assets/Scripts/TerrainMovment.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class TerrainMovment : MonoBehaviour {
 
-	private float distance = 0;
-	[SerializeField] private GameObject terrain;
-	[SerializeField] private float speed;
+	[SerializeField] private Transform back;
+	private float speed;
 
-	void Update () {
-		distance = terrain.transform.position.z;
-		if(distance > -100)
+	private void Update()
+	{
+		speed = GameManager.Instance.TerrainSpeed;
+		if(transform.position.z > -100)
 		{
-			terrain.transform.Translate((Vector3.back * speed * Time.deltaTime));
-		}else
-		{
-			distance = 199;
-			terrain.transform.Translate(Vector3.forward * (distance));
+			transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+		}else{
+			if (transform.position.z != back.position.z)
+			{
+				transform.position = back.TransformDirection(back.position);
+			}
 		}
 	}
 }
