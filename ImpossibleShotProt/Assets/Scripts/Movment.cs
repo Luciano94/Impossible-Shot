4f81ac2204e0a2b8617c7b4ec0efa466191c3062
Unknown <lucho.elp40@gmail.com>
2018-08-26 00:18:47 -0300

    v0.3-Spawner
    -Added spawn prefab
    -fixed terrain error
    -Added properties to the game manager

---
+++ ImpossibleShotProt/Assets/Scripts/Movment.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment : MonoBehaviour {
+
+	[SerializeField] private float lifeTime; //tiempo de vida del objeto
+
+	private void Awake()
+	{
+		Destroy(gameObject, lifeTime);
+	}
+
+	void Update () {
+		transform.Translate(Vector3.back * GameManager.Instance.TerrainSpeed * Time.deltaTime);
+	}
+}
---
+++ ImpossibleShotProt/Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	[SerializeField] private GameObject prefab;		//prefab del objeto a spawnear
+	private float timePerWave;						//tiempo entre spawns
+	[SerializeField] private Vector2 spawnMinValues;//valores minimos de aparicion en x e y
+	[SerializeField] private Vector2 spawnMaxValues;//valores maximos de aparicion en x e y
+
+	// Use this for initialization
+	void Awake() {
+		timePerWave = GameManager.Instance.TimePerEnemy;
+		Invoke("SpawnWave", timePerWave);
+	}
+
+	private void Update()
+	{
+		timePerWave = GameManager.Instance.TimePerEnemy;
+	}
+
+	void SpawnWave()
+	{
+		Vector3 spawnPosition = new Vector3(Random.Range(spawnMinValues.x, spawnMaxValues.x), Random.Range(spawnMinValues.y, spawnMaxValues.y), transform.position.z);
+		Instantiate(prefab, spawnPosition, Quaternion.identity);
+		Invoke("SpawnWave", timePerWave);
+	}
+}
