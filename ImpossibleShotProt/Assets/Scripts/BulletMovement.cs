98b50333c779ce2c7592d8910f73f6cb4f2bc365
Unknown <tmugetti@gmail.com>
2018-08-26 00:49:04 -0300

    v0.3 Bullet & Camera Movement
    -Bullet movement (No bounds or collisions)
    -Camera movement

---
+++ ImpossibleShotProt/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+
+	[SerializeField] float Speed;
+
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direc = Vector3.zero;
+		direc += Vector3.up * Input.GetAxis ("Vertical") * Speed * Time.deltaTime;
+		direc += Vector3.right * Input.GetAxis ("Horizontal") * Speed * Time.deltaTime;
+		transform.Translate (direc);
+
+	}
+}
---
+++ ImpossibleShotProt/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+	[SerializeField] Transform Bullet;
+	[SerializeField] float Speed;
+	[SerializeField] float DifferenceWindow;
+	[SerializeField] Transform Up;
+	[SerializeField] Transform Left;
+	[SerializeField] Transform Right;
+
+	Vector3 NormalVector;
+
+	private void Start(){
+		//set the initial camera-to-bullet distance and angle as the standard
+		NormalVector = transform.position - Bullet.position; 
+	}
+
+	private void LateUpdate () {
+		Vector3 BulletPos = Bullet.position;
+		Vector3 MyPos = transform.position;
+		Vector3 direc = Vector3.zero;
+		//Left-Right axis
+		//Left
+		if(MyPos.x > BulletPos.x && MyPos.x - BulletPos.x > DifferenceWindow){
+			if (MyPos.x >= Left.position.x) {
+				direc += Vector3.left * Speed * Time.deltaTime;
+			} else {
+				transform.LookAt (Bullet);
+			}
+		//Right
+		} else if((MyPos.x < BulletPos.x && BulletPos.x - MyPos.x > DifferenceWindow)){
+			if (MyPos.x <= Right.position.x) {
+				direc += Vector3.right * Speed * Time.deltaTime;
+			} else {
+				transform.LookAt (Bullet);
+			}
+		}
+		//Up-Down axis
+		MyPos.y += NormalVector.y;
+		//Down
+		if(MyPos.y < BulletPos.y && BulletPos.y - MyPos.y > DifferenceWindow){
+			direc += Vector3.down * Speed * Time.deltaTime;
+		//Up
+		} else if(MyPos.y > BulletPos.y && MyPos.y - BulletPos.y > DifferenceWindow){
+			
+			if (MyPos.y <= Up.position.y) {
+				direc += Vector3.up * Speed * Time.deltaTime;
+			} else {
+				transform.LookAt (Bullet);
+			}
+		}
+		//FinalMovement
+		transform.Translate(direc);
+	}
+}
