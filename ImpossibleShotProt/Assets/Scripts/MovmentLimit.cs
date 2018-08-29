e6c20c2ac21604fbb0ccddb1de0aa068fe1cd14d
Unknown <lucho.elp40@gmail.com>
2018-08-28 22:06:32 -0300

    v0.7-MovementLimits
    -Added the movement limits for the character

---
+++ ImpossibleShotProt/Assets/Scripts/MovmentLimit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovmentLimit : MonoBehaviour {
+
+	[SerializeField]private Vector2 limits; 
+
+	void Update () {
+		if (transform.position.x > limits.x)
+			transform.position = new Vector3 (limits.x, transform.position.y, transform.position.y);
+		else if (transform.position.x < (-limits.x))
+			transform.position = new Vector3 ((-limits.x), transform.position.y, transform.position.y);
+		if (transform.position.y > limits.y)
+			transform.position = new Vector3 (transform.position.x, limits.y, transform.position.z);
+		else if (transform.position.y < 0.5f)
+			transform.position = new Vector3 (transform.position.x, 0.5f, transform.position.z);
+	}
+}
