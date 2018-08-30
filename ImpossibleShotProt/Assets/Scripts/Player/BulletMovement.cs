1d38208ddc849a75fcf9524a0cab529bfaf00706
Unknown <lucho.elp40@gmail.com>
2018-08-30 15:56:08 -0300

    v0.11-BulletRotation

--- ImpossibleShotProt/Assets/Scripts/Player/BulletMovement.cs
+++ ImpossibleShotProt/Assets/Scripts/Player/BulletMovement.cs
@@ -13,9 +13,18 @@ public class BulletMovement : MonoBehaviour {
 		Vector3 direc = Vector3.zero;
 		direc += Vector3.up * Input.GetAxis ("Vertical") * Speed * Time.deltaTime;
 		direc += Vector3.right * Input.GetAxis ("Horizontal") * Speed * Time.deltaTime;
-		Vector3 rot = new Vector3(-direc.y, direc.x,0);
-		transform.Rotate(rot);	
 		transform.Translate (direc);
+		Rotate();
+	}
 
+	private void Rotate(){
+		if (Input.GetAxis("Vertical") == 0)
+			transform.rotation = new Quaternion(0, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+		else
+			transform.rotation = new Quaternion(-(0.1f * Input.GetAxis("Vertical")),transform.rotation.y, transform.rotation.z, transform.rotation.w);
+		if (Input.GetAxis("Horizontal") == 0)
+			transform.rotation = new Quaternion(transform.rotation.x,0, transform.rotation.z, transform.rotation.w);
+		else
+			transform.rotation = new Quaternion(transform.rotation.x, (0.1f * Input.GetAxis("Horizontal")), transform.rotation.z, transform.rotation.w);
 	}
 }
