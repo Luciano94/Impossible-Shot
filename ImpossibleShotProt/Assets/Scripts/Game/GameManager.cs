5064e341edb13b20495718fd307b3133849340ef
Unknown <lucho.elp40@gmail.com>
2018-09-06 23:23:47 -0300

    v0.17-Gameplay
    -Agregado flow de gameplay final

--- ImpossibleShotProt/Assets/Scripts/Game/GameManager.cs
+++ ImpossibleShotProt/Assets/Scripts/Game/GameManager.cs
@@ -21,15 +21,21 @@ public class GameManager : MonoBehaviour {
 		}
 	}
 
-	[SerializeField] private float terrainSpeed = 20;		//Velocidad del terreno y enemigos
+	[SerializeField] private float terrainSpeed = 50;		//Velocidad del terreno y enemigos
 	[SerializeField] private float speedPerLevel = 10;		//cantidad de aceleracion por nivel
 	[SerializeField] private float timePerLevel = 30;		//duracion de cada nivel
 	[SerializeField] private float timePerEnemy = 5;		//tiempo entre cada enemigo en los spawners
 	[SerializeField] private float enemyTimePerLevel = 1;   //tiempo que se resta en el spawner en cada nivel
-	[SerializeField] private float speedPerEnemy = 0.5f;	//aumento de velocidad por enemigo eliminado
-	private float timeCurrentLevel;							//tiempo que transcurrio en el nivel actual
+	[SerializeField] private float speedPerEnemy = 0.5f;	//aumento de velocidad por enemigo eliminado					
 	[SerializeField] private float fieldOfView;
     [SerializeField] private float fieldPerLevel;
+    private float timeCurrentLevel;                         //tiempo que transcurrio en el nivel actual
+    private float NextFOV;
+    private bool acceleration = false;
+    private float targetSpeed;
+    private float baseFOV;
+    private float baseSpeed;
+
 	public float TimePerEnemy
 	{
 		get
@@ -37,11 +43,13 @@ public class GameManager : MonoBehaviour {
 			return timePerEnemy;
 		}
 	}
+	
 	public float FOV{
 		get{
 			return fieldOfView;
 		}
 	}
+	
 	public float TerrainSpeed
 	{
 		get
@@ -52,7 +60,9 @@ public class GameManager : MonoBehaviour {
 
 	public void Aceleration()
 	{
-		terrainSpeed += speedPerEnemy;
+        targetSpeed = terrainSpeed + speedPerEnemy;
+        NextFOV = Camera.main.fieldOfView + fieldPerLevel;
+		acceleration = true;
 	}
 
 	public void Death()
@@ -63,20 +73,37 @@ public class GameManager : MonoBehaviour {
 	private void Awake()
 	{
 		timeCurrentLevel = 0;
-		fieldOfView = Camera.main.fieldOfView;
+		baseFOV = Camera.main.fieldOfView;
+        baseSpeed = 0f;
+		acceleration = false;
 	}
 
-	private void Update()
-	{
+    private void LateUpdate() {
+		LevelControl();
+		AcceleracionControl();
+    }
+
+	private void LevelControl(){
 		timeCurrentLevel += Time.deltaTime;
-		if(timeCurrentLevel >= timePerLevel)
-		{
-			timeCurrentLevel = 0;
-			terrainSpeed += speedPerLevel;
-			if (timePerEnemy > 1)
-				timePerEnemy -= enemyTimePerLevel;
-			else timePerEnemy = 0.5f;
-			//fieldOfView = Mathf.Lerp(fieldOfView, fieldOfView + fieldPerLevel, 0.01f);
-		}
+        if(timeCurrentLevel >= timePerLevel) {
+            timeCurrentLevel = 0;
+            timePerEnemy -= enemyTimePerLevel;
+            if(timePerEnemy < 1)
+                timePerEnemy = 0.5f;
+        }
+	}
+
+	private void AcceleracionControl(){
+		if(acceleration) {
+            terrainSpeed = Mathf.Lerp(terrainSpeed, targetSpeed+0.5f, 0.9f * Time.deltaTime);
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, NextFOV, 0.9f * Time.deltaTime);
+            if(terrainSpeed >= targetSpeed)
+                acceleration = false;
+        }
+        else {
+            terrainSpeed = Mathf.Lerp(terrainSpeed, baseSpeed, 0.2f * Time.deltaTime);
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, baseFOV, 0.9f * Time.deltaTime);
+        }
 	}
 }
+
