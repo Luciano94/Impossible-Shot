c5ea13802e03fb74af52e53a308060f76189848b
Unknown <tmugetti@gmail.com>
2018-09-19 00:11:28 -0300

    v0.33 - Movement grid

--- ImpossibleShotProt/Assets/Scripts/Player/BulletMovement.cs
+++ ImpossibleShotProt/Assets/Scripts/Player/BulletMovement.cs
@@ -4,47 +4,46 @@ using UnityEngine;
 
 public class BulletMovement : MonoBehaviour {
 
-	float speed;
-	private GameManager gM;
-	
+	//float speed;
+	//private GameManager gM;
+
+	[SerializeField] Transform MatrixCenter;
+	[SerializeField] float DistanceToCenter;
+	[SerializeField][Range (0f,1f)] float IgnorableDistance;
+	[SerializeField][Range (0f,1f)] float TransitionSharpness;
+	int x = 0;
+	int y = 0;
+	int yNext = 0;
+	int xNext = 0;
+	bool moving = false;
 	private void Awake() {
-		gM=GameManager.Instance;
-		speed = gM.BulletSpeed;
+		/*gM=GameManager.Instance;
+		speed = gM.BulletSpeed;*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//new movement control
-		DirectionVec dir =  InputManager.Instance.GetDirection();
-		int x = 0;
-		int y = 0;
-		switch (dir.y) {
-		case Direction.Up:
-			y = 1;
-			break;
-		case Direction.Down:
-			y = -1;
-			break;
-		default:
-			break;
+		//si no me estoy ya moviento
+		if (!moving) {
+			//detecto orden de movimiento
+			moveDetection();
+			if(xNext != x || yNext != y){
+				moving = true; //de ser necesario comienzo a moverme;
+			}
+		} else {
+			moveAction ();
 		}
-
-		switch (dir.x){
-		case Direction.Right:
-			x = 1;
-			break;
-		case Direction.Left:
-			x = -1;
-			break;
-		default:
-			break;
-		}
+		//detecto cambios de movimiento
+		/*
 		//old movement
 		Vector3 direc = Vector3.zero;
 		direc += Vector3.up * y * speed * Time.deltaTime;
 		direc += Vector3.right * x * speed * Time.deltaTime;
 		transform.Translate (direc);
 		//Rotate(x,y);
+		*/
+
 	}
 
 	private void Rotate(int x, int y){
@@ -56,5 +55,64 @@ public class BulletMovement : MonoBehaviour {
 			transform.rotation = new Quaternion(transform.rotation.x,0, transform.rotation.z, transform.rotation.w);
 		else
 			transform.rotation = new Quaternion(transform.rotation.x, (0.1f * x), transform.rotation.z, transform.rotation.w);
+	}
+
+	private void moveDetection(){
+		DirectionVec dir = InputManager.Instance.GetDirection ();
+		//Debug.Log (dir.x + " " + dir.y );
+		switch (dir.y) {
+		case Direction.Up:
+			yNext += 1;
+			break;
+		case Direction.Down:
+			yNext -= 1;
+			break;
+		default:
+			break;
+		}
+
+		switch (dir.x) {
+		case Direction.Right:
+			xNext += 1;
+			break;
+		case Direction.Left:
+			xNext -= 1;
+			break;
+		default:
+			break;
+		}
+		if (xNext > 1) {xNext = 1;}
+		if (xNext < -1) {xNext = -1;}
+		if (yNext > 1) {yNext = 1;}
+		if (yNext < -1) {yNext = -1;}
+	}
+
+	private void moveAction(){
+		float yTarget = MatrixCenter.position.y + (DistanceToCenter * yNext);
+		float xTarget = MatrixCenter.position.x + (DistanceToCenter * xNext);
+
+		/*bool xFix = false;
+		bool yFix = false;
+
+		if(((xTarget > transform.position.x) && xTarget - transform.position.x <= IgnorableDistance) || 
+			((xTarget < transform.position.x) && transform.position.x - xTarget <= IgnorableDistance)){
+			xFix = true;
+		}
+
+		if(((yTarget > transform.position.y) && yTarget - transform.position.y <= IgnorableDistance) || 
+			((yTarget < transform.position.y) && transform.position.y - yTarget <= IgnorableDistance)){
+			yFix = true;
+		}
+
+		Debug.Log (xFix + "" + yFix);*/
+		Vector3 Target = new Vector3 (xTarget, yTarget, transform.position.z);
+		if (Vector3.Distance(transform.position,Target) <= IgnorableDistance) {
+			transform.position = Target;
+			y = yNext;
+			x = xNext;
+			moving = false;
+		} else {
+			transform.position = Vector3.Lerp (transform.position, new Vector3 (xTarget, yTarget, transform.position.z), TransitionSharpness);
+		}
 	}
 }
