0e016d1b7d0a4453dc19ec491788aae18a96657c
Unknown <lucho.elp40@gmail.com>
2018-09-27 14:35:57 -0300

    Spawn Enviroment

---
+++ ImpossibleShotProt/Assets/Scripts/Enviroment/EnvScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvScript : MonoBehaviour {
+	[SerializeField] float distZ;
+	private GameManager gameManager;
+	private float speed;
+
+	private void Awake() {
+		gameManager = GameManager.Instance;
+	}
+	private void LateUpdate() {
+		speed = gameManager.TerrainSpeed;
+		transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+		if(transform.position.z < distZ)
+			Destroy(gameObject);
+	}
+}
---
+++ ImpossibleShotProt/Assets/Scripts/Enviroment/SpawnEnv.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnv : MonoBehaviour {
+
+	[SerializeField] GameObject prefab;
+	[SerializeField] float timePerObj;
+
+	private void Awake() {
+		Invoke("Spawn", timePerObj);
+	}
+
+	private void Spawn(){
+		Instantiate(prefab, transform.position, transform.rotation);
+		Invoke("Spawn", timePerObj);
+	}
+}
