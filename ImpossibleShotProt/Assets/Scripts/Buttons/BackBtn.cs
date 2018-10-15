eb205648c1f075dd994dbf3ec9d551935a994fa9
Unknown <tmugetti@gmail.com>
2018-10-15 18:18:30 -0300

    How To Play panel and buttons

---
+++ ImpossibleShotProt/Assets/Scripts/Buttons/BackBtn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackBtn : MonoBehaviour {
+
+	[SerializeField] private GameObject CurrentPanel;
+	[SerializeField] private GameObject PanelToReturnTo;
+
+	public void Clicked(){
+		CurrentPanel.SetActive(false);
+		PanelToReturnTo.SetActive(true);
+	}
+}
---
+++ ImpossibleShotProt/Assets/Scripts/Buttons/HowToBtn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToBtn : MonoBehaviour {
+
+	[SerializeField] GameObject HowToPanel;
+	[SerializeField] GameObject CurrentPanel;
+
+	public void Clicked(){
+		CurrentPanel.SetActive(false);
+		HowToPanel.SetActive(true);
+	}
+}
