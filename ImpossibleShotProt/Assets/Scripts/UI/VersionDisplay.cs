afb709712de5057de6440642f0765b417a48442e
Unknown <tmugetti@gmail.com>
2018-10-18 14:21:16 -0300

    Version display
    minor fixes on UI, script folder "buttons" is now called "UI", bullet normal map error fixed.

---
+++ ImpossibleShotProt/Assets/Scripts/UI/VersionDisplay.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VersionDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<UnityEngine.UI.Text>().text += Application.version;
+	}
+}
