8ce17f177ac2796de0095a5f10faf1f803aa5a3a
Unknown <tmugetti@gmail.com>
2018-10-23 22:35:34 -0300

    PanelButton script  updated
    Other button scripts' names corrected

--- ImpossibleShotProt/Assets/Scripts/UI/PanelButton.cs
+++ ImpossibleShotProt/Assets/Scripts/UI/PanelButton.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PanelButton : MonoBehaviour {
-	[SerializeField] GameObject Current;
-	[SerializeField] GameObject Next;
+	[SerializeField] GameObject[] Current;
+	[SerializeField] GameObject[] Next;
 	
 	public void Clicked(){
-		Next.SetActive(true);
-		Current.SetActive(false);
+		if (Current.Length > 0){
+			for (int i = 0; i < Current.Length ;i++){
+				Current[i].SetActive(false);
+			}
+		}
+
+		if(Next.Length > 0){
+			for(int i = 0; i < Next.Length; i++){
+				Next[i].SetActive(true);
+			}
+		}
 	}
 }
