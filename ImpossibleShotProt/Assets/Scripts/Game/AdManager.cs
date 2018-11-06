36fb42daa133d0a7f85f436a666ef9af96e411c6
Unknown <tmugetti@gmail.com>
2018-11-05 22:22:44 -0300

    Unity monetization plugin

---
+++ ImpossibleShotProt/Assets/Scripts/Game/AdManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdManager : MonoBehaviour {
+
+	private static AdManager instance = null;
+
+    public static AdManager Instance {
+        get {
+            instance = FindObjectOfType<AdManager>();
+            if(instance == null) {
+                GameObject go = new GameObject("Ad Manager");
+                instance = go.AddComponent<AdManager>();
+            }
+            return instance;
+        }
+    }
+
+	void Awake(){
+		DontDestroyOnLoad(gameObject);
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
