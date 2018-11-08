0c40569a13b2512a29191fe60879e27b31a2b8c6
Unknown <tmugetti@gmail.com>
2018-11-08 17:43:49 -0300

    Fog effect on enemies, obstacles and terrain
    ground fog sub optimal

--- ImpossibleShotProt/Assets/Scripts/Enviroment/SpawnEnv.cs
+++ ImpossibleShotProt/Assets/Scripts/Enviroment/SpawnEnv.cs
@@ -1,15 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnEnv : MonoBehaviour {
 
-	public static SpawnEnv instance;
+    public static SpawnEnv instance;
 
-	   public static SpawnEnv Instance {
+    public static SpawnEnv Instance {
         get {
             instance = FindObjectOfType<SpawnEnv>();
-            if(instance == null) {
+            if (instance == null) {
                 GameObject go = new GameObject("SpawnEnv");
                 instance = go.AddComponent<SpawnEnv>();
             }
@@ -17,42 +16,42 @@ public class SpawnEnv : MonoBehaviour {
         }
     }
 
-	[SerializeField] GameObject[] prefabs;
-	[SerializeField] float timePerObj;
-	[SerializeField] int pool = 50;
-	private Queue<GameObject> envArray;
-	int random;
+    [SerializeField] GameObject[] prefabs;
+    [SerializeField] float timePerObj;
+    [SerializeField] int pool = 50;
+    private Queue<GameObject> envArray;
+    int random;
 
-	public void UpdateTime(){
-		timePerObj -= 0.02f;
-		if(timePerObj <= 0.1f)
-			timePerObj =0.1f;
-	}
-	
-	private void Awake() {
-		envArray = new Queue<GameObject>();
-		GameObject go;
-		for(int i=0; i < pool;i++){
-			random = Random.Range(0, prefabs.Length-1);
-			go = Instantiate(prefabs[random],transform.position,transform.rotation);
-			go.SetActive(false);
-			envArray.Enqueue(go);
-		}
-		Invoke("Spawn", timePerObj);
-	}
+    public void UpdateTime() {
+        timePerObj -= 0.02f;
+        if (timePerObj <= 0.1f)
+            timePerObj = 0.1f;
+    }
 
-	private void Spawn(){
-		GameObject go;
-		if(envArray.Count > 0){
-			go = envArray.Dequeue();
-			go.SetActive(true);
-		}
-		Invoke("Spawn", timePerObj);
-	}
+    private void Awake() {
+        envArray = new Queue<GameObject>();
+        GameObject go;
+        for (int i = 0; i < pool; i++) {
+            random = Random.Range(0, prefabs.Length - 1);
+            go = Instantiate(prefabs[random], transform.position, transform.rotation);
+            go.SetActive(false);
+            envArray.Enqueue(go);
+        }
+        Invoke("Spawn", timePerObj);
+    }
 
-	public void Despawn(GameObject go){
-		go.transform.position = transform.position;
-		go.SetActive(false);
-		envArray.Enqueue(go);
-	}
+    private void Spawn() {
+        GameObject go;
+        if (envArray.Count > 0) {
+            go = envArray.Dequeue();
+            go.SetActive(true);
+        }
+        Invoke("Spawn", timePerObj);
+    }
+
+    public void Despawn(GameObject go) {
+        go.transform.position = transform.position;
+        go.SetActive(false);
+        envArray.Enqueue(go);
+    }
 }
---
+++ ImpossibleShotProt/Assets/TerrainSpriteFogEffect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (SpriteRenderer))]
+public class TerrainSpriteFogEffect : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+    private Color initialColor;
+    private Color targetColor;
+    private float initialPosition;
+    private float targetPosition;
+    
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        initialColor = spriteRenderer.color;
+        targetColor = RenderSettings.fogColor;
+
+        initialPosition = RenderSettings.fogEndDistance;
+        targetPosition = RenderSettings.fogStartDistance;
+        if (targetPosition < 0) { targetPosition = 0; }
+	}
+	
+	void Update () {
+        if (transform.position.z >= 0 && (initialPosition - targetPosition) != 0 ) {
+            float lerpState = transform.position.z / (initialPosition - targetPosition);
+            spriteRenderer.color = Color.Lerp(initialColor, Color.black, lerpState);
+        }
+	}
+}
