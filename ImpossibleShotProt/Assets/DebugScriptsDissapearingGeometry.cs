271bc31fb2a9dbb9591dd0cf7129cd692acd527f
Unknown <tmugetti@gmail.com>
2018-11-15 16:58:19 -0300

    Fixed #5
    Product script foind on geometry.

---
+++ ImpossibleShotProt/Assets/DebugScriptsDissapearingGeometry.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugScriptsDissapearingGeometry : MonoBehaviour {
+
+    private void OnDestroy()
+    {
+        Debug.Log("destroyed");
+    }
+    private void OnDisable()
+    {
+        Debug.Log("disabled");
+    }
+    private void OnTransformParentChanged()
+    {
+        Debug.Log("Parent changed");
+    }
+}
