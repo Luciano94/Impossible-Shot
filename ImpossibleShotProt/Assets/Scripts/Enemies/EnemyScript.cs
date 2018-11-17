4c3cd25c891e09d07732a9ce3bce11cd7403f2cf
Unknown <lucho.elp40@gmail.com>
2018-11-17 01:23:57 -0300

    Fixed #7

--- ImpossibleShotProt/Assets/Scripts/Enemies/EnemyScript.cs
+++ ImpossibleShotProt/Assets/Scripts/Enemies/EnemyScript.cs
@@ -3,11 +3,9 @@
 public class EnemyScript : MonoBehaviour {
 	
 	[SerializeField] int points = 100;
-	private bool death = false;
 
 	private void OnTriggerEnter(Collider other){
-		if (!death && other.gameObject.tag != "TutorialCollider" ){
-			death = true;
+		if (other.gameObject.tag != "TutorialCollider" ){
 			GameManager.Instance.EnemyDeath(points);
 		}
 	}
