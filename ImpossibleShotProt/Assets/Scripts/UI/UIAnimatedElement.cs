fcde78e628822fb51db5c69023b5ae1831a55775
Unknown <tmugetti@gmail.com>
2018-11-28 05:47:13 -0300

    UII animations

---
+++ ImpossibleShotProt/Assets/Scripts/UI/UIAnimatedElement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class UIAnimatedElement : MonoBehaviour {
+
+	[SerializeField] private float speed = 1;
+	[SerializeField] private bool moves;
+	[SerializeField] private float initialX;
+	[SerializeField] private float initialY;
+
+
+	private Vector2 initialPositionVector;
+	private Vector2 targetPositionVector;
+	private RectTransform rectTransform;
+	private Button button;
+	private bool wasInteractable;
+	private bool animating;
+	private float lerpState;
+
+	private UIElementAnimation director;
+	private int elementID;
+	void Start () {
+		button = GetComponent<Button>();
+		if(button){ wasInteractable = button.interactable; button.interactable = false;}
+	
+
+		rectTransform = GetComponent<RectTransform>();
+		if(moves){
+			initialPositionVector = new Vector2(initialX * 100, initialY * 100);
+			targetPositionVector = rectTransform.anchoredPosition;
+			rectTransform.anchoredPosition = initialPositionVector;
+		}
+		animating = false;
+		lerpState = 0;
+	}
+
+	void Update () {
+		if(animating){
+			lerpState += Time.unscaledDeltaTime * speed * 2;
+			if(lerpState >= 1){Stop(); return;}
+			if(moves){
+				rectTransform.anchoredPosition = Vector3.Lerp(initialPositionVector,targetPositionVector,lerpState);
+			}
+		}
+	}
+
+	public void Animate(){
+		animating = true;
+	}
+
+	void Stop(){
+		director.animationDone(elementID);
+		animating = false;
+		rectTransform.anchoredPosition = targetPositionVector;
+		if(button){ button.interactable = wasInteractable;}
+	}
+
+	public void SetAnimation(UIElementAnimation animator, int ID){
+		director = animator;
+		elementID = ID;
+	}
+
+	public void Reset(){
+		lerpState = 0;
+		if(moves){ rectTransform.anchoredPosition = initialPositionVector;}
+		if(button){ wasInteractable = button.interactable; button.interactable = false;}
+	}
+}
---
+++ ImpossibleShotProt/Assets/Scripts/UI/UIElementAnimation.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class UIElementAnimation : MonoBehaviour {
+	[SerializeField] private UIAnimatedElement[] elements;
+	[SerializeField] private bool[] shouldNextWait;
+	private bool[] doneAnimating;
+	private bool animating = false;
+
+	private void Start(){
+		doneAnimating = new bool[elements.Length];
+		for(int i = 0; i < elements.Length; i++){
+			doneAnimating[i] = false;
+			elements[i].SetAnimation(this,i);
+		}
+		shouldNextWait[shouldNextWait.Length-1] = false;
+	}
+
+	void OnEnable(){
+		animating = true;
+	}
+	void OnDisable(){
+		for(int i = 0; i < elements.Length; i++){
+			elements[i].Reset();
+			doneAnimating[i] = false;
+		}
+	}
+	
+	public void animationDone(int ID){
+		doneAnimating[ID] = true;
+	}
+
+	private bool AllDone(){
+		for(int i = 0; i < elements.Length; i++){
+			if(doneAnimating[i] == false){ return false;}
+		}
+		return true;
+	}
+
+	void LateUpdate(){
+		if(animating){
+			for(int i = 0; i < elements.Length; i++){
+				if(!doneAnimating[i]){
+					elements[i].Animate();
+					if(shouldNextWait[i]){ return;}
+				}
+			}
+			if(AllDone()){animating = false;}
+		}
+	}
+}
