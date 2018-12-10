187d9f6380ae196f3fac0e466267d9d2137acd57
Unknown <lucho.elp40@gmail.com>
2018-12-10 16:05:12 -0300

    added the button to turn on and off the music

---
+++ ImpossibleShotProt/Assets/Scripts/UI/MusicConfig.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicConfig : MonoBehaviour {
+
+	[SerializeField] Sprite musicOn;
+	[SerializeField] Sprite musicOff;
+	private Button musicBtn;
+	public bool musicOnOff ;
+
+	private void Start() {
+		musicOnOff = true;
+		musicBtn = GetComponent<Button>();
+	}
+
+	public void MusicOnOff(){
+		if(musicOnOff){
+			musicOnOff = false;
+			//wwise mute
+			musicBtn.image.sprite = musicOff;
+		}else{
+			musicOnOff = true;
+			//wwise sound
+			musicBtn.image.sprite = musicOn;
+		}
+	}
+}
