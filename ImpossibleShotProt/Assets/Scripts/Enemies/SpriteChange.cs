13df2192e0c57842fd186e7acf2ffe27ebf2afd7
Unknown <lucho.elp40@gmail.com>
2018-12-12 12:59:35 -0300

    now the enemies have an animation before they die

---
+++ ImpossibleShotProt/Assets/Scripts/Enemies/SpriteChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChange : MonoBehaviour {
+
+	[SerializeField] Sprite enemyIdle;
+	[SerializeField] Sprite enemyScreaming;
+	private SpriteRenderer enemySprite;
+	private bool isScreaming;
+
+	private void Start() {
+		enemySprite = GetComponentInChildren<SpriteRenderer>();
+		isScreaming = false;
+	}
+
+	public void ChangeSprite(){
+		if(isScreaming)
+			enemySprite.sprite = enemyIdle;
+		else
+			enemySprite.sprite = enemyScreaming;
+	}
+
+	public void Reset(){
+		Invoke("ChangeSprite", 3.0f);
+	}
+}
---
+++ ImpossibleShotProt/Assets/Scripts/Player/RayEnemyDetection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayEnemyDetection : MonoBehaviour {
+
+	private Ray detectionRay;
+	private RaycastHit raycastHit;
+
+	private void Start() {
+		detectionRay = new Ray(transform.position, transform.forward * 10);
+	}
+
+	private void LateUpdate() {
+		detectionRay.origin = transform.position;
+		if(Physics.Raycast(detectionRay, out raycastHit)){
+			if(raycastHit.distance < 10)
+				if(raycastHit.transform.gameObject.tag == "Enemy"){
+					ChangeSprite(raycastHit.transform.gameObject.GetComponent<SpriteChange>());
+				}
+		}
+	}
+
+	private void ChangeSprite(SpriteChange spr){
+		spr.ChangeSprite();
+		spr.Reset();
+	}
+	
+}
